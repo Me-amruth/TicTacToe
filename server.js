da931c0aa0bbd9a8f3122d2bd1b153226c8cc7ec
Amruth <amruthhh@outlook.com>
2024-11-29 18:22:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    path: '/socket.io', // Default path
+});
+
 
 app.use(express.static('public'));
 
